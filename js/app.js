219afbc918989a8701c3335f55435bdc815a967d
btgormley <47070560+btgormley@users.noreply.github.com>
2020-07-30 11:48:02 -0400

    JavaScript Functionality
    -Added basic JS functionality
    -Array of displays started
    -Function to check if input value matches array index
    -Displays if match or not

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-let displayText = document.querySelector('.main-input').value
+// JS VARIABLES (DOM)
+let itemInput = document.querySelector('.main-input');
+let displayText = document.querySelector('.main-display-text');
+let mainButton = document.querySelector('.main-button');
+
+// APPLIANCE DISPLAYS ARRAY
+let applianceDisplays = ['852884'];
+
+
+
+mainButton.addEventListener('click', () => {
+    checkDisplay();
+});
+
+
+
+
+
+
+
+
+// CHECKS ITEM NUMBER IF ON DISPLAY
+function checkDisplay() {
+    for (let i = 0; i < applianceDisplays.length; i++) {
+        if (itemInput.value === applianceDisplays[i]) {
+            displayText.textContent = 'DISPLAYED!';
+        }
+        else {
+            displayText.textContent = 'NOT DISPLAYED :(';
+        }
+    }
+}
+
+
+
