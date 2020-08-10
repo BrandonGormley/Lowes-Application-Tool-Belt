f0816b74e6dc31c53998a031f81ce4ab2090f7e0
btgormley <47070560+btgormley@users.noreply.github.com>
2020-08-10 11:57:33 -0400

    File Restructure p.2 & Tool Belt Creation

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// JS VARIABLES (DOM)
+let itemInput = document.querySelector('.main-input');
+let displayText = document.querySelector('.main-display-text');
+let mainButton = document.querySelector('.main-button');
+
+// APPLIANCE DISPLAYS ARRAY
+let applianceDisplays = ['852884'];
+
+
+
+mainButton.addEventListener('click', () => {
+    checkDisplay();
+});
+
+
+
+
+
+
+
+
+// CHECKS ITEM NUMBER IF ON DISPLAY
+function checkDisplay() {
+    for (let i = 0; i < applianceDisplays.length; i++) {
+        if (itemInput.value === applianceDisplays[i]) {
+            displayText.textContent = `${itemInput.value} is DISPLAYED!`;
+        }
+        else {
+            displayText.textContent = `Appliance is NOT DISPLAYED :(`;
+        }
+    }
+}
+
+
+
