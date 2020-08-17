9148410fd1dbda732a2455ff71dcb17fc70b95f6
btgormley <47070560+btgormley@users.noreply.github.com>
2020-08-17 15:44:34 -0400

    Appliance Check
    -Filled out Display list
    -Added example items

--- Appliance-Display/app.js
+++
@@ -1,7 +0,0 @@
-let weight = document.querySelector('#weight');
-let button = document.querySelector('.water-button');
-let waterIntake = document.querySelector('#water-intake');
-
-button.addEventListener('click', () => {
-    waterIntake.textContent = parseInt(weight.value) * .67 + ' fluid ounces daily.';
-});
