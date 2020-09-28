1b6866ba764b6f774453eb4ccdd48dd15eb26064
btgormley <47070560+btgormley@users.noreply.github.com>
2020-09-28 13:23:29 -0400

    JS Added
    -Added most of the JS for Granite Selection
    -Fully functional to use

--- CounterTop-Calculator/js/app.js
+++ CounterTop-Calculator/js/app.js
@@ -9,4 +9,31 @@ let squareFootInput = document.querySelector('.main-input');
 let calculateButton = document.querySelector('.main-button');
 let total = document.querySelector('#total');
 
+const costOfA = 50;
+const costOfB = 60;
+const costOfC = 70;
+const costOfD = 80;
+const costOfE = 90;
+
+let letterButtons = [buttonA, buttonB, buttonC, buttonD, buttonE];
+let counterTopPrices = [costOfA, costOfB, costOfC, costOfD, costOfE];
+
+for (let i = 0; i < letterButtons.length; i++) {
+    letterButtons[i].addEventListener('click', () => {
+        letterButtons[i].classList.toggle('button-selected');
+    });
+}
+
+
+for (let i = 0; i < letterButtons.length; i++) {
+    for (let j = 0; j < counterTopPrices.length; j++) {
+        calculateButton.addEventListener('click', () => {
+            if (letterButtons[i].classList.contains('button-selected')) {
+                let totalCost = squareFootInput.value * counterTopPrices[i];
+                total.innerHTML = `Estimated Cost: $${totalCost}`;
+            }
+        });
+    }
+}
+
 
